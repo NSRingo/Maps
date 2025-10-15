98303290799783dfbbb6c60a66c1eae0bad5e593
脆薯饼 <Virgil@nanocat.me>
2025-10-15 11:55:04 +0800

    fix: 调整PLIST配置中的PedestrianAREnabled和OpticalHeadingEnabled的注释状态以优化功能

--- src/response.dev.js
+++ src/response.dev.js
@@ -96,8 +96,8 @@ Console.info(`PLATFORM: ${PLATFORM}`);
 								PLIST["com.apple.GEO"].CountryProviders.CN.GEOShouldSpeakWrittenPlaceNames = true; // TW
 								// US
 								PLIST["com.apple.GEO"].CountryProviders.CN["6694982d2b14e95815e44e970235e230"] = true; // US
-								//PLIST["com.apple.GEO"].CountryProviders.CN.PedestrianAREnabled = true; // 现实世界中的线路
-								PLIST["com.apple.GEO"].CountryProviders.CN.OpticalHeadingEnabled = true; // 举起以查看
+								PLIST["com.apple.GEO"].CountryProviders.CN.PedestrianAREnabled = true; // 现实世界中的线路-举起以查看
+								//PLIST["com.apple.GEO"].CountryProviders.CN.OpticalHeadingEnabled = true; // 举起以查看
 								//PLIST["com.apple.GEO"].CountryProviders.CN.UseCLPedestrianMapMatchedLocations = true; // 导航准确性-增强
 								// Public
 								PLIST["com.apple.GEO"].Q0FFNUI2QUEtRUU2.push("CN");
@@ -109,7 +109,6 @@ Console.info(`PLATFORM: ${PLATFORM}`);
 								PLIST["com.apple.GEO"].NavigationShowHeadingKey = true; // 导航显示方向键?
 								PLIST["com.apple.GEO"].SupportsCarIntegration = true; // 支持 CarPlay 集成
 								//PLIST["com.apple.GEO"]["6694982d2b14e95815e44e970235e230"] = true; // US
-								//PLIST["com.apple.GEO"].PedestrianAREnabled = true; // 现实世界中的线路
 								//PLIST["com.apple.GEO"].OpticalHeadingEnabled = true; // 举起以查看
 								PLIST["com.apple.GEO"].UseCLPedestrianMapMatchedLocations = true; // 导航准确性-增强
 							}
