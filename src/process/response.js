77f74bc9e648f250cebe0c8df3aa7e2f84d93b06
001ProMax <101964350+001ProMax@users.noreply.github.com>
2026-03-06 04:15:53 +0800

    fix: type error

--- src/process/response.js
+++ src/process/response.js
@@ -51,7 +51,7 @@ export async function Response($request, $response) {
                     BigInt.prototype.toJSON = function () {
                         return this.toString();
                     };
-                    body = XML.parse($response.body);
+                    body = XML.parse($app === "Node.js" ? new TextDecoder().decode($response.body ?? new Uint8Array()) : $response.body);
                     // 路径判断
                     switch (url.pathname) {
                         case "/config/defaults": {
@@ -94,7 +94,7 @@ export async function Response($request, $response) {
             break;
         case "text/json":
         case "application/json":
-            body = JSON.parse($response.body ?? "{}");
+            body = $app === "Node.js" ? new TextDecoder().decode($response.body ?? new Uint8Array()) : JSON.parse($response.body ?? "{}");
             Console.debug(`body: ${JSON.stringify(body)}`);
             $response.body = JSON.stringify(body);
             break;
